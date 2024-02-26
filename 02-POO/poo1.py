38ed7324e4cec18aedb0fd059201f5cc1f08d091
Happenedspice17 <emilianoeg1@hotmail.com>
2024-02-26 11:07:13 -0600

    modified:   02-POO/poo1.py

--- 02-POO/poo1.py
+++ 02-POO/poo1.py
@@ -21,14 +21,36 @@ class Person:
         self.hobbies = []
         self.height = height
 
-    def say_hello(self):
+    def say_hello(self) -> None:
         print("Hello, my name is " +  self.name)
 
-    def say_age(self):
+    def say_age(self) -> None:
         print("I am " + str(self.age) + " years old")
 
-    def birthday(self):
+    def birthday(self) -> None:
         self.age += 1
 
     def add_hobby(self, hobby: str) -> None:
         self.hobbies.append(hobby)
+
+class Point2D:
+    def __init__(self) -> None:
+        self.x_pos = 0
+        self.y_pos = 0
+
+    #* good practice code
+    def set_x(self, x: float) -> None:
+        self.x_pos = x
+
+    def set_y(self, y: float) -> None:
+        self.y_pos = y
+
+    def get_x(self) -> float:
+        return self.x_pos
+    
+    def get_y(self) -> float:
+        return self.y_pos
+
+
+my_point = Point2D()
+my_point.set_x(10)
